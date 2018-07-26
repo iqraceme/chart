26b472d3c3efec955bba8a000a424c1eac62d296
Rahul Bothra <f2016015@pilani.bits-pilani.ac.in>
2018-07-26 14:00:19 +0530

    remove state keyword to popup calls

--- activity.py
+++ activity.py
@@ -641,7 +641,7 @@ class ChartActivity(activity.Activity):
 
     def _measure_btn_clicked(self, button):
         palette = button.get_palette()
-        palette.popup(immediate=True, state=1)
+        palette.popup(immediate=True)
 
     def _add_value(self, widget, label='', value='0.0'):
         if label == '':
